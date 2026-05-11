0617bbaf45459629c558045588e111a6ef7cc859
VictorChsse <118536539+VChasse@users.noreply.github.com>
2026-05-11 14:43:54 +0200

    Update Eartquake_Simulation.py

--- pages/Eartquake_Simulation.py
+++ pages/Eartquake_Simulation.py
@@ -1,18 +1,9 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
-with open("pages/carte_villages_seismes.html", "r", encoding="utf-8") as file:
-    html_content = file.read()
+with open("pages/carte_villages_seismes.html", "r", encoding="utf-8") as f:
+    html_content = f.read()
 
-st.markdown(
-    f"""
-    <iframe
-        srcdoc='{html_content}'
-        width="100%"
-        height="900"
-        style="border:none;"
-    ></iframe>
-    """,
-    unsafe_allow_html=True,
-)
+components.html(html_content, height=900, scrolling=True)
